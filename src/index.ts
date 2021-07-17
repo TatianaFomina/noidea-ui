bea3e2fe6f6072a0ccf743bad02db38771e36bb4
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2021-07-17 15:51:30 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './index.css'
+
 export { default as Button } from './components/button/button.vue'
 export { default as Input } from './components/input/input.vue'
 export { default as Spinner } from './components/spinner/spinner.vue'
