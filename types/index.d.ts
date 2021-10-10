ada47f8a355695d12936de7515c5b7dad33b621d
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2021-10-10 12:21:58 +0300

    Rename package

---
+++ types/index.d.ts
@@ -0,0 +1,1 @@
+declare module 'noidea-ui'
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib'
+      name: 'noidea-ui'
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
